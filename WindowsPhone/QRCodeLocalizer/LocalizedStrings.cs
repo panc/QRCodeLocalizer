3f9f7002abb64c6dacb3a8838dda42d3f119e52c
panc <christoph.pangerl@gmail.com>
2013-01-27 20:09:46 +0100

    Added WindowsPhone project.

---
+++ WindowsPhone/QRCodeLocalizer/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using QRCodeLocalizer.Resources;
+
+namespace QRCodeLocalizer
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static readonly AppResources sLocalizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return sLocalizedResources; } }
+    }
+}
